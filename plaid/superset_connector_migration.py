3a5fc5cd7b2e076bba3a632a9e288dad8be89d0a
Garrett Bates <garrett.bates@tartansolutions.com>
2020-01-06 03:05:34 -0500

    adding migration script

---
+++ plaid/superset_connector_migration.py
@@ -0,0 +1,117 @@
+import logging
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+
+from superset import db
+from superset.connectors.plaid.models import PlaidTable, PlaidProject, PlaidMetric
+from superset.connectors.sqla.models import SqlaTable, SqlMetric
+from superset.models.core import Slice
+
+log = logging.getLogger(__name__)
+log.setLevel('INFO')
+
+def add_changeme_datasource():
+    proj = db.session.query(PlaidProject).filter_by(uuid="placeholder_project").first()
+    if not proj:
+        proj = PlaidProject()
+
+        proj.name = "placeholder_project"
+        proj.uuid = "placeholder_project"
+        proj.workspace_id = "placeholder_workspace"
+        proj.workspace_name = "placeholder_workspace"
+        proj.password = "placeholder_project"
+
+        driver = "postgresql"
+        host = "google.com"
+        user = "bogus"
+        port = "5432"
+        db_name = "bogus"
+
+        # Construct URI and use sqla mapping method to set it.
+        uri = f"{driver}://{user}:{proj.password}@{host}:{port}/{db_name}"
+        proj.set_sqlalchemy_uri(uri)
+
+        db.session.add(proj)
+        db.session.commit()
+
+    table = PlaidTable()
+
+    table.table_name = "change_me"
+    table.friendly_name = "change_me"
+    table.project_id = "placeholder_project"
+    table.project = proj
+    table.schema = "placeholder"
+
+    db.session.add(table)
+    db.session.commit()
+
+
+def update_charts_to_new_datasource():
+    charts = db.session.query(Slice).filter_by(datasource_type='table').all()
+    placeholder_table = db.session.query(PlaidTable).filter(
+        PlaidTable.project_id == 'placeholder_project',
+        PlaidTable.schema == 'placeholder',
+        PlaidTable.table_name == 'change_me',
+    ).one()
+    for chart in charts:
+        log.info(f'Updating {chart.slice_name}...')
+        chart.datasource_type = 'plaid'
+        try:
+            old_table = db.session.query(SqlaTable).get(chart.datasource_id)
+            new_table = db.session.query(PlaidTable).filter(
+                PlaidTable.table_name == old_table.table_name,
+                PlaidTable.schema == old_table.schema,
+            ).one()
+
+            chart.datasource_id = new_table.id
+            db.session.commit()
+
+        except NoResultFound:
+            log.info(f'No result found for {old_table.schema}.{old_table.table_name}')
+            chart.datasource_id = placeholder_table.id
+            db.session.commit()
+        except Exception:
+            log.exception('idk what happened.')
+            db.session.rollback()
+
+
+def migrate_metrics():
+    old_tables = db.session.query(SqlaTable).all()
+
+    for old_table in old_tables:
+        try:
+            new_table = db.session.query(PlaidTable).filter(
+                PlaidTable.table_name == old_table.table_name,
+                PlaidTable.schema == old_table.schema,
+            ).one()
+            log.info(f"Found matching table for {old_table}. Porting metrics now.")
+            try:
+                for old_metric in old_table.metrics:
+
+                    if db.session.query(
+                        db.session.query(PlaidMetric).filter(
+                            PlaidMetric.metric_name == old_metric.metric_name,
+                            PlaidMetric.table_id == new_table.id
+                        ).exists()
+                    ).scalar():
+                        continue
+                    new_metric = PlaidMetric()
+                    new_metric.metric_name = old_metric.metric_name
+                    new_metric.verbose_name = old_metric.verbose_name
+                    new_metric.metric_type = old_metric.metric_type
+                    new_metric.table_id = new_table.id
+                    new_metric.expression = old_metric.expression
+                    new_metric.description = old_metric.description
+                    new_metric.warning_text = old_metric.warning_text
+                    db.session.add(new_metric)
+                    db.session.commit()
+                    log.info(f"Copying {old_metric.metric_name}...")
+            except Exception:
+                log.warning(f"Something happend. Idk what")
+                db.session.rollback()
+        except NoResultFound:
+            log.warning(f"Table {old_table} does not exist. Continuing.")
+
+
+if __name__ == '__main__':
+    update_charts_to_new_datasource()
