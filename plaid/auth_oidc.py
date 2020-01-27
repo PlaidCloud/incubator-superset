774555290c7c9a0c153fb2c58f00db6ef0a33221
Garrett Bates <garrett.bates@tartansolutions.com>
2020-01-27 18:49:56 -0500

    add missing imports and fix formatting

--- plaid/auth_oidc.py
+++ plaid/auth_oidc.py
@@ -1,6 +1,8 @@
+from urllib.parse import quote
+from flask import redirect, request
 from flask_appbuilder.security.views import AuthOIDView
+from flask_appbuilder import expose
 from flask_login import login_user
-from urllib import quote
 
 class AuthOIDCView(AuthOIDView):
 
@@ -10,17 +12,17 @@ class AuthOIDCView(AuthOIDView):
         oidc = sm.oid
 
         @self.appbuilder.sm.oid.require_login
-        def handle_login(): 
+        def handle_login():
             user = sm.auth_user_oid(oidc.user_getfield('email'))
 
             if user is None:
                 info = oidc.user_getinfo(['preferred_username', 'given_name', 'family_name', 'email'])
-                user = sm.add_user(info.get('preferred_username'), info.get('given_name'), info.get('family_name'), info.get('email'), sm.find_role('Gamma')) 
+                user = sm.add_user(info.get('preferred_username'), info.get('given_name'), info.get('family_name'), info.get('email'), sm.find_role('Gamma'))
 
             login_user(user, remember=False)
-            return redirect(self.appbuilder.get_url_for_index)  
+            return redirect(self.appbuilder.get_url_for_index)
 
-    return handle_login()  
+        return handle_login()
 
     @expose('/logout/', methods=['GET', 'POST'])
     def logout(self):
@@ -28,7 +30,7 @@ class AuthOIDCView(AuthOIDView):
         oidc = self.appbuilder.sm.oid
 
         oidc.logout()
-        super(AuthOIDCView, self).logout()        
+        super(AuthOIDCView, self).logout()
         redirect_url = request.url_root.strip('/') + self.appbuilder.get_url_for_login
 
         return redirect(oidc.client_secrets.get('issuer') + '/protocol/openid-connect/logout?redirect_uri=' + quote(redirect_url))
