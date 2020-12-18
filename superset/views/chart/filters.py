9ea18b536c3654b31120d1827915a2cb3e0a298e
Garrett Bates <garrett.bates@tartansolutions.com>
2020-12-18 10:51:18 -0500

    compare table UUIDs, not chart UUIDs

--- superset/views/chart/filters.py
+++ superset/views/chart/filters.py
@@ -31,5 +31,5 @@ class SliceFilter(BaseFilter):  # pylint: disable=too-few-public-methods
         schema_perms = security_manager.user_view_menu_names("schema_access")
         table_uuids = security_manager.table_uuids_for_session()
         return query.filter(
-            or_(self.model.perm.in_(perms), self.model.schema_perm.in_(schema_perms), self.model.uuid.in_(table_uuids)),
+            or_(self.model.perm.in_(perms), self.model.schema_perm.in_(schema_perms), self.model.table.uuid.in_(table_uuids)),
         )
