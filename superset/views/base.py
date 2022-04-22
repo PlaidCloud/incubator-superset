ac511c5a9cbccb84576e702e39fcbd349059706d
Adams Tower <adams.tower@tartansolutions.com>
2022-04-22 09:57:45 -0400

    More instrumentation

--- superset/views/base.py
+++ superset/views/base.py
@@ -411,6 +411,7 @@ def show_superset_errors(ex: SupersetErrorsException) -> FlaskResponse:
 # Redirect to login if the CSRF token is expired
 @superset_app.errorhandler(CSRFError)
 def refresh_csrf_token(ex: CSRFError) -> FlaskResponse:
+    logger.info(request)
     logger.error(ex)
 
     if request.is_json:
