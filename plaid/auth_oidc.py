4438a688ff255dde963fa199856312c660d61505
Adams Tower <adams.tower@tartansolutions.com>
2022-05-06 13:00:31 -0400

    try session.clear instead of response.delete_cookie

--- plaid/auth_oidc.py
+++ plaid/auth_oidc.py
@@ -49,7 +49,8 @@ class AuthOIDCView(AuthOIDView):
         response = make_response(redirect('/'))
         # TODO: probably parameterize cookie name, though I suspect it won't change.
         response.delete_cookie('_session_id', path='/', domain=domain)
-        response.delete_cookie('session', path='/', domain='visualize.plaidcloud.net')
+        session.clear()
+        # response.delete_cookie('session', path='/', domain='visualize.plaidcloud.net')
         return response
 
 def throwaway_password() -> str:
