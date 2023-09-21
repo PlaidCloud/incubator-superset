75aeabf50c21b257c5447a9bdc68152ff605577a
Adams Tower <adams.tower@tartansolutions.com>
2023-09-21 11:31:31 -0400

    Remove incorrect useTruncation import

--- superset-frontend/src/dashboard/components/nativeFilters/FilterCard/DependenciesRow.tsx
+++ superset-frontend/src/dashboard/components/nativeFilters/FilterCard/DependenciesRow.tsx
@@ -20,7 +20,7 @@ import React, { useCallback, useMemo, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { css, t, useTheme, useTruncation } from '@superset-ui/core';
 import Icons from 'src/components/Icons';
-import { useTruncation } from 'src/hooks/useTruncation';
+// import { useTruncation } from 'src/hooks/useTruncation';
 import { setDirectPathToChild } from 'src/dashboard/actions/dashboardState';
 import {
   DependencyItem,
