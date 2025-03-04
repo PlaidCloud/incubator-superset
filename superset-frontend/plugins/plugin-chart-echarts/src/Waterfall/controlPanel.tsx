88fa45e93f1ed06f2f17a54c77929547b829d696
anantaoutlook <ananta.subhash@outlook.com>
2025-03-04 13:11:05 +0000

    modify waterfall chart

--- superset-frontend/plugins/plugin-chart-echarts/src/Waterfall/controlPanel.tsx
+++ superset-frontend/plugins/plugin-chart-echarts/src/Waterfall/controlPanel.tsx
@@ -16,32 +16,72 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import React from 'react';
-import { hasGenericChartAxes, t } from '@superset-ui/core';
+import { t } from '@superset-ui/core';
 import {
   ControlPanelConfig,
   ControlSubSectionHeader,
   D3_TIME_FORMAT_DOCS,
   DEFAULT_TIME_FORMAT,
   formatSelectOptions,
-  sections,
   sharedControls,
 } from '@superset-ui/chart-controls';
+import React from 'react';
 import { showValueControl } from '../controls';
 
 const config: ControlPanelConfig = {
   controlPanelSections: [
-    sections.genericTime,
     {
       label: t('Query'),
       expanded: true,
       controlSetRows: [
-        [hasGenericChartAxes ? 'x_axis' : null],
-        [hasGenericChartAxes ? 'time_grain_sqla' : null],
+        ['x_axis'],
+        ['time_grain_sqla'],
         ['groupby'],
         ['metric'],
         ['adhoc_filters'],
         ['row_limit'],
+        [
+          {
+            name: 'seriesOrderByColumn',
+            config: {
+              type: 'SelectControl',
+              label: t('Order Series By Column'),
+              description: t(
+                'Column to use for ordering the waterfall series with columns not in the chart',
+              ),
+              mapStateToProps: state => ({
+                choices: [
+                  [null, t('None')],
+                  ...(state.datasource?.columns || []).map(col => [
+                    col.column_name,
+                    col.column_name,
+                  ]),
+                ],
+              }),
+              default: null,
+              renderTrigger: true,
+            },
+          },
+        ],
+        [
+          {
+            name: 'seriesOrderDirection',
+            config: {
+              type: 'SelectControl',
+              label: t('Order Direction'),
+              choices: [
+                [null, t('None')],
+                ['ASC', t('Ascending')],
+                ['DESC', t('Descending')],
+              ],
+              default: null,
+              renderTrigger: true,
+              description: t(
+                'Ordering direction for the series, to be used with "Order Series By Column"',
+              ),
+            },
+          },
+        ],
       ],
     },
     {
@@ -58,6 +98,58 @@ const config: ControlPanelConfig = {
               renderTrigger: true,
               default: false,
               description: t('Whether to display a legend for the chart'),
+            },
+          },
+        ],
+        [
+          {
+            name: 'show_total',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Show Total'),
+              default: true,
+              renderTrigger: true,
+              description: t('Show the total value in the waterfall chart'),
+            },
+          },
+        ],
+        [
+          {
+            name: 'bold_total',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Bold Total'),
+              default: true,
+              renderTrigger: true,
+              description: t(
+                'Bold the total axis label in the waterfall chart',
+              ),
+            },
+          },
+        ],
+        [
+          {
+            name: 'useFirstValueAsSubtotal',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Use first value as subtotal'),
+              default: false,
+              renderTrigger: true,
+              description: t('Render the first bar in the chart as a subtotal'),
+            },
+          },
+        ],
+        [
+          {
+            name: 'bold_sub_total',
+            config: {
+              type: 'CheckboxControl',
+              label: t('Bold first value as subtotal'),
+              default: true,
+              renderTrigger: true,
+              description: t(
+                'Bold the first bar axis label in the waterfall chart',
+              ),
             },
           },
         ],
@@ -109,6 +201,20 @@ const config: ControlPanelConfig = {
         ],
         [
           {
+            name: 'x_axis_label_distance',
+            config: {
+              type: 'TextControl',
+              label: t('X Axis Label Distance'),
+              description: t('Distance of the label from X axis (in pixels)'),
+              renderTrigger: true,
+              default: '25',
+              isInt: true,
+              // validators: [v => !Number.isNaN(v) && v >= 0],
+            },
+          },
+        ],
+        [
+          {
             name: 'x_axis_time_format',
             config: {
               ...sharedControls.x_axis_time_format,
@@ -137,6 +243,39 @@ const config: ControlPanelConfig = {
             },
           },
         ],
+        [
+          {
+            name: 'sort_x_axis',
+            config: {
+              type: 'SelectControl',
+              label: t('Sort X Axis'),
+              default: 'none',
+              choices: [
+                ['none', t('None')],
+                ['asc', t('Ascending')],
+                ['desc', t('Descending')],
+              ],
+              renderTrigger: true,
+              description: t('Sort X axis in ascending or descending order'),
+            },
+          },
+        ],
+        [
+          {
+            name: 'orientation',
+            config: {
+              type: 'SelectControl',
+              label: t('Orientation'),
+              default: 'vertical',
+              choices: [
+                ['vertical', t('Vertical')],
+                ['horizontal', t('Horizontal')],
+              ],
+              renderTrigger: true,
+              description: t('Orientation of the chart'),
+            },
+          },
+        ],
         [<ControlSubSectionHeader>{t('Y Axis')}</ControlSubSectionHeader>],
         [
           {
@@ -146,6 +285,20 @@ const config: ControlPanelConfig = {
               label: t('Y Axis Label'),
               renderTrigger: true,
               default: '',
+            },
+          },
+        ],
+        [
+          {
+            name: 'y_axis_label_distance',
+            config: {
+              type: 'TextControl',
+              label: t('Y Axis Label Distance'),
+              description: t('Distance of the label from Y axis (in pixels)'),
+              renderTrigger: true,
+              default: '25',
+              isInt: true,
+              // validators: [v => !Number.isNaN(v) && v >= 0],
             },
           },
         ],
