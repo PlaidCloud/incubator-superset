cf52c6f7f13bdb56ec41a352a7155e80b15fd8e9
anantaoutlook <ananta.subhash@outlook.com>
2025-04-02 10:37:27 +0000

    fix import error MainPreset.js, rest prettier changes

--- superset-frontend/plugins/plugin-chart-marimekko/src/plugin/controlPanel.ts
+++ superset-frontend/plugins/plugin-chart-marimekko/src/plugin/controlPanel.ts
@@ -99,7 +99,6 @@ const config: ControlPanelConfig = {
 
   // For control input types, see: superset-frontend/src/explore/components/controls/index.js
   controlPanelSections: [
-    
     {
       label: t('Query'),
       expanded: true,
