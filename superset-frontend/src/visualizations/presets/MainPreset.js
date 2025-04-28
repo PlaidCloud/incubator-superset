eeaf702fec44de254ddd678de52112d82d9eff88
anantaoutlook <ananta.subhash@outlook.com>
2025-04-28 07:39:47 +0000

    add plugin whale chart, add column sorting to mekko, fix category labels mekko, fix scatterplot color not displaying according to categories in dashboard

--- superset-frontend/src/visualizations/presets/MainPreset.js
+++ superset-frontend/src/visualizations/presets/MainPreset.js
@@ -80,6 +80,9 @@ import { PivotTableChartPlugin as PivotTableChartPluginV2 } from '@superset-ui/p
 import { HandlebarsChartPlugin } from '@superset-ui/plugin-chart-handlebars';
 import { FilterPlugins } from 'src/constants';
 import TimeTableChartPlugin from '../TimeTable';
+import { PluginChartMarimekko } from '@superset-ui/plugin-chart-marimekko';
+import { BigTextChart } from '@superset-ui/plugin-chart-big-text';
+import { SupersetPluginChartWhale } from '@superset-ui/plugin-chart-whale';
 
 export default class MainPreset extends Preset {
   constructor() {
@@ -185,6 +188,9 @@ export default class MainPreset extends Preset {
           ],
         }).configure({ key: VizType.Cartodiagram }),
         ...experimentalPlugins,
+        new SupersetPluginChartWhale().configure({
+          key: 'ext-whale',
+        }),
       ],
     });
   }
