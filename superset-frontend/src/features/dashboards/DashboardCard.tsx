90c001e1339d95fcaaa0e1efb2a061288098fe8e
Pat Buxton <patrick.buxton@tartansolutions.com>
2025-06-09 15:57:12 +0100

    Alternative dashboard URl fix

--- superset-frontend/src/features/dashboards/DashboardCard.tsx
+++ superset-frontend/src/features/dashboards/DashboardCard.tsx
@@ -92,6 +92,7 @@ function DashboardCard({
         endpoint: `/api/v1/dashboard/${dashboard.id}`,
       }).then(({ json = {} }) => {
         setThumbnailUrl(json.result?.thumbnail_url || '');
+        //dashboard.thumbnail_url = json.result?.thumbnail_url || '';
         setFetchingThumbnail(false);
       });
     }
@@ -161,7 +162,7 @@ function DashboardCard({
         }
         url={bulkSelectEnabled ? undefined : dashboard.url}
         linkComponent={Link}
-        imgURL={dashboard.thumbnail_url}
+        imgURL={thumbnailUrl}
         imgFallbackURL="/static/assets/images/dashboard-card-fallback.svg"
         description={t('Modified %s', dashboard.changed_on_delta_humanized)}
         coverLeft={<FacePile users={dashboard.owners || []} />}
