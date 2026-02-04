822421e26a5c6fbb5add7b54841514b6432fb0a6
anantanoutlook <parthmanhas@gmail.com>
2026-02-04 18:35:21 +0530

    copy mekko changes from 6.0 to here

--- superset-frontend/plugins/plugin-chart-marimekko/src/types.ts
+++ superset-frontend/plugins/plugin-chart-marimekko/src/types.ts
@@ -55,4 +55,5 @@ export type PluginChartMarimekkoProps = PluginChartMarimekkoStylesProps &
     tooltipTimeFormat?: string;
     tooltipShowPercentage?: boolean;
     tooltipIncludeColumn?: boolean;
+    showXAxisLabels?: boolean;
   };
